8a471d629ad6aeffab4c2eac6cf5b2a5f0d9e199
alaska113 <alaska113@gmail.com>
2023-12-28 12:07:21 -0800

    Add menu functionality for current chord. Add chord appending to progression system

--- src/components/Chords.tsx
+++ src/components/Chords.tsx
@@ -1,36 +1,86 @@
-import { Chip } from "@mui/material";
-import { useNotes } from "../utils/ChordContext";
-import { useChords } from "../utils/ProgressionContext";
-import { Chord } from "../types/Chord";
-import NoteAnalysis from "../analysis/NoteAnalysis";
-
+import { Chip, Menu, MenuItem } from '@mui/material';
+import { useNotes } from '../utils/ChordContext';
+import { useChords } from '../utils/ProgressionContext';
+import { Chord } from '../types/Chord';
+import NoteAnalysis from '../analysis/NoteAnalysis';
+import { MouseEvent, useCallback, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 interface ChordChipProps {
     chord: Chord;
 }
 
+const CurrentChord: React.FC = () => {
+    const { chordNotes, clearNotes } = useNotes();
+    const { addChord } = useChords();
+    const noteAnalysis = new NoteAnalysis();
+    const identifiedChord = noteAnalysis.identifyChord(chordNotes);
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event: MouseEvent<HTMLDivElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+
+    const addChordToProgression = useCallback(() => {
+        addChord({
+            id: uuidv4(),
+            name: identifiedChord,
+            description: '',
+            notes: chordNotes
+        })
+    }, [addChord, chordNotes])
+
+    if (identifiedChord) {
+        return (
+            <div>
+                <Chip
+                    label={identifiedChord}
+                    variant="outlined"
+                    onClick={handleClick}
+                />
+                <Menu
+                    id="new-chord-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                        'aria-labelledby': 'basic-button'
+                    }}
+                >
+                    <MenuItem onClick={addChordToProgression}>Add Chord</MenuItem>
+                    <MenuItem>Add Chord To...</MenuItem>
+                    <MenuItem onClick={() => clearNotes()}>Clear</MenuItem>
+                </Menu>
+            </div>
+        );
+    }
+
+    return null;
+};
+
 const ChordChip: React.FC<ChordChipProps> = ({ chord }) => {
     const { removeChord } = useChords();
-    return <Chip
-        label={chord.name}
-        onClick={() => removeChord(chord.id)}
-    />
-}
+    return <Chip label={chord.name} onClick={() => removeChord(chord.id)} />;
+};
 
 const Chords: React.FC = () => {
-    const { chordNotes } = useNotes();
     const { progressionChords } = useChords();
-    const noteAnalysis = new NoteAnalysis();
-
-    const identifiedChord = noteAnalysis.identifyChord(chordNotes);
-    console.log('identifiedChord: ', identifiedChord);
     return (
         <div>
-            {identifiedChord && <Chip label={identifiedChord} variant='outlined'/>}
+            <CurrentChord />
             <div>
-                {progressionChords.map((chord, index) => <ChordChip chord={chord} key={index}/>)}
+                {progressionChords.map((chord, index) => (
+                    <ChordChip chord={chord} key={index} />
+                ))}
             </div>
         </div>
     );
-}
+};
 
 export default Chords;
